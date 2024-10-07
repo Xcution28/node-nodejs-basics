4cebc85cdf0387169960ec4eb90f071861555a75
Xcution <xcution.js@gmail.com>
2024-10-08 03:46:22 +0700

    implement args.js and env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,19 @@
 const parseEnv = () => {
-    // Write your code here 
+    // get all environment variables
+    const variables = process.env;
+
+    // filtering variables with 'RSS_' prefix
+    const rss_variables = Object.entries(variables)
+        .filter(([key]) => key.startsWith('RSS_'))
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ');
+
+    // checking is there variables with 'RSS_'
+    if (rss_variables) {
+        console.log(rss_variables);
+    } else {
+        console.log('No RSS_ variables');
+    }
 };
 
 parseEnv();
